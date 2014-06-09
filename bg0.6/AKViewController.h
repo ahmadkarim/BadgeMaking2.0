9c1530de2a6c714e99cd484be7e1a53c19d14f3f
ahmadkarim <ahmadkarim@Ahmads-MacBook-Pro.local>
2014-06-10 04:22:29 +0500

    i have modified the AKViewController , and have teased camera , it takes pictures and save them in cgimagecontext , which can be printed later on .

--- bg0.6/AKViewController.h
+++ bg0.6/AKViewController.h
@@ -7,7 +7,45 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <UIKit/UIKit.h>
+#import <CoreVideo/CoreVideo.h>
+#import <CoreMedia/CoreMedia.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface AKViewController : UIViewController
+@interface AKViewController : UIViewController<AVCaptureVideoDataOutputSampleBufferDelegate>{
+    
+    UIImage * imageToResize;
+    UIImage * CropImage;
+ 
+    UIImage * buffer;
+    IBOutlet UIButton *NextScreenButtonOutlet;
+    IBOutlet UIButton *CaptureButtonOutlet;
+    IBOutlet UIButton *SwitchButtonOutlet;
+    IBOutlet UIImageView *CameraScreenReplace;
+    
+
+    
+    IBOutlet UIButton *OriginalbuttonOutlet;
+    IBOutlet UIButton *ToonButtonOutlet;
+    IBOutlet UIButton *CropButtonOutlet;
+    IBOutlet UIImageView *MessagesViewController;
+    
+    BOOL ToonFilterActivated;
+    BOOL OriginalImageSelected;
+    BOOL isUsingFrontFacingCamera;
+}
+//BUffer
+
+@property (weak, nonatomic) IBOutlet UIImageView *TestImage;
+
+// Camera
+@property (weak, nonatomic) IBOutlet UIImageView* cameraImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *CroppedImageView;
+@property (strong, nonatomic) AVCaptureDevice* device;
+@property (strong, nonatomic) AVCaptureSession* captureSession;
+@property (strong, nonatomic) AVCaptureVideoPreviewLayer* previewLayer;
+@property (strong, nonatomic) UIImage* cameraImage;
+@property (weak, nonatomic) UIView* CroppedView;
+@property (strong, nonatomic)UIImage * CropImage;
 
 @end
